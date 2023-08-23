f1467313bffbbd9d0d77fb4c46eb92b1452afe31
Hitnei <nhihoang@tenomad.com>
2023-08-23 23:44:21 +0700

    custom controller

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -1,9 +1,34 @@
 'use strict';
-
 /**
- * category controller
+ *  category controller
  */
-
 const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::category.category', ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    const query = strapi.db.query('api::category.category');
+    await Promise.all(
+      data.map(async (item, index) => {
+        const foundItem = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ['createdBy', 'updatedBy'],
+        });
 
-module.exports = createCoreController('api::category.category');
+        data[index].attributes.createdBy = {
+          id: foundItem.createdBy.id,
+          firstname: foundItem.createdBy.firstname,
+          lastname: foundItem.createdBy.lastname,
+        };
+        data[index].attributes.updatedBy = {
+          id: foundItem.updatedBy.id,
+          firstname: foundItem.updatedBy.firstname,
+          lastname: foundItem.updatedBy.lastname,
+        };
+      })
+    );
+    return { data, meta };
+  },
+}));
